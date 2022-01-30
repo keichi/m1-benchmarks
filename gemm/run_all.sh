1d9d618bac63c45b44c7554a130582e5b4506152
Keichi Takahashi <hello@keichi.dev>
2022-01-30 18:47:21 +0900

    Update benchmak scripts

--- gemm/run_all.sh
+++ gemm/run_all.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
-mkdir -p result
+if [ $# -ne 1 ]; then
+    echo "Usage: ./run_all.sh <result_dir>"
+    exit 1
+fi
+
+RESULT_DIR=$1
 
 echo "Benchmarking with Accelerate"
 mkdir -p build-accelerate
 pushd build-accelerate
 cmake -DDTYPE=FLOAT -DBACKEND=Accelerate -DCMAKE_BUILD_TYPE=Release ..
 make
-./gemm_bench > ../result/accelerate.dat
 popd
+build-accelerate/gemm_bench > "$RESULT_DIR/accelerate.dat"
 
 echo "Benchmarking with OpenBLAS"
 mkdir -p build-openblas
 pushd build-openblas
 LDFLAGS=-L$(brew --prefix openblas)/lib cmake -DDTYPE=FLOAT -DBACKEND=OpenBLAS -DCMAKE_BUILD_TYPE=Release ..
 CPATH=$(brew --prefix openblas)/include make
-./gemm_bench > ../result/openblas.dat
 popd
+build-openblas/gemm_bench > "$RESULT_DIR/openblas.dat"
 
 echo "Benchmarking with Eigen"
 mkdir -p build-eigen
 pushd build-eigen
 cmake -DDTYPE=FLOAT -DBACKEND=Eigen -DCMAKE_BUILD_TYPE=Release ..
 make
-./gemm_bench > ../result/eigen.dat
 popd
+build-eigen/gemm_bench > "$RESULT_DIR/eigen.dat"
 
 echo "Benchmarking with Metal"
 mkdir -p build-metal
 pushd build-metal
 cmake -DDTYPE=FLOAT -DBACKEND=Metal -DCMAKE_BUILD_TYPE=Release ..
 make
-./gemm_bench > ../result/metal.dat
 popd
+build-metal/gemm_bench > "$RESULT_DIR/metal.dat"
 
-pushd result
+cd "$RESULT_DIR"
 gnuplot plot_gemm.gpi
--- stream/run_all.sh
+++ stream/run_all.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-mkdir -p result
+if [ $# -ne 1 ]; then
+    echo "Usage: ./run_all.sh <result_dir>"
+    exit 1
+fi
+
+RESULT_DIR=$1
 
 echo "Benchmarking with Metal"
 pushd MetalStream
 xcodebuild
-./build/Release/MetalStream > ../result/metal.csv
 popd
+MetalStream/build/Release/MetalStream > "$RESULT_DIR/metal.csv"
 
 echo "Benchmarking with OpenMP"
 pushd BabelStream
 make -f OpenMP.make COMPILER=CLANG TARGET=CPU
-OMP_NUM_THREADS=1 ./omp-stream --float --csv > ../result/openmp1.csv
-OMP_NUM_THREADS=4 ./omp-stream --float --csv > ../result/openmp4.csv
-OMP_NUM_THREADS=8 ./omp-stream --float --csv > ../result/openmp8.csv
+popd
+OMP_NUM_THREADS=1 BabelStream/omp-stream --float --csv > $RESULT_DIR/openmp1.csv
+OMP_NUM_THREADS=4 BabelStream/omp-stream --float --csv > $RESULT_DIR/openmp4.csv
+OMP_NUM_THREADS=8 BabelStream/omp-stream --float --csv > $RESULT_DIR/openmp8.csv
+
+cd "$RESULT_DIR"
+gnuplot plot_stream.gpi
